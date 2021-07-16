678ce57e288fbb999aee1b204417df1171cad058
Geoffry Millner <geoff.millner22@gmail.com>
2021-07-16 15:40:14 -0700

    make bootstrap function properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 function Game(player1Object, player2Object) {
     this.player1 = player1Object;
     this.player2 = player2Object;
-    this.currentPlayer = 1
+    this.currentPlayer = 1;
     this.round = 1;
+    this.currentScore = 0;
 };
 
 
 
 function Player(name) {
     this.name = name
-    this.score = 0
+    this.totalScore = 0
 }
 
 // to make turns for user player
@@ -17,14 +18,20 @@ Game.prototype.turn = function() {
 
 }
 // to be used to build computer strategy
-Game.prototype.computerAi = function() {
+Player.prototype.computerAi = function() {
 
 }
 
 //victory or loss conditions/reset of game
-Game.prototype.End = function() {
-    if (this.score === 100)
-}
+Player.prototype.checkScore = function() {
+    return this.totalScore >= 100 ? true : false;
+    
+    // if (this.totalScore >= 100){
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+};
 
 // these below are for computer and player
 let newPlayer = new Player("jim")
@@ -40,5 +47,9 @@ const diceRollMath = Math.floor(Math.random() * 6) + 1;
 // const mathForTurn = Math.floor(Math.random() * 2) + 1;
 
 console.log(diceRollMath);
-console.log(getRandom())
+console.log(getRandom());
+console.log(rollDie());
+console.log(newPlayer);
+console.log(computerPlayer);
+console.log(currentGame);
 
